c3bd1a8a0ca6d9dfd7ac516e3c5f81848eb46e90
Rohit Singh <rs31321@gmail.com>
2024-04-15 18:19:03 +0530

    enhance UI

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -32,11 +32,13 @@ const Weather = () => {
         return `${day} ${date} ${month} ${year}`
     }
   return (
-    <div>
-    <main>
-        <div className='search-box'>
+    <div className={(typeof weather.main != "undefined") 
+    ? ((weather.main.temp > 16) 
+        ? 'app warm' : 'app'): 'app'}>
+    <main className='container h-screen w-screen  mx-auto text-center '>
+        <div className='search-box flex justify-center items-center'>
             <input type="text" 
-                className='search-bar w-64 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500' 
+                className='search-bar w-3/5 mt-20  my-4 py-4 px-4 text-lg rounded-md focus:outline-none focus:ring focus:border-blue-500 shadow-lg' 
                 placeholder='Search...'
                 value={query}
                 onChange={e => setQuery(e.target.value)}
@@ -44,20 +46,22 @@ const Weather = () => {
         </div>
         {(typeof weather.main != "undefined")?(
             <div>
-            <div className="location-box">
-                <div className="location">
+            <div className="location-box m-20">
+                <div className="location text-6xl font-medium tracking-wider">
                 {weather.name}, {weather.sys.country}
                 </div>
-                <div className="date">
+                <div className="date font-mono text-xl mt-2">
                 {dateBuilder(new Date())}
                 </div>
             </div>
-            <div className="weather-box">
-                <div className="temp">{Math.round(weather.main.temp)}°C</div>
+            <div className="weather-box px-10 py-6  brightness-125 backdrop-blur-sm bg-white/30 border border-blue-200 inline-block rounded-xl shadow-lg shadow-indigo-500/50 ">
+                <div className="temp text-7xl font-bold">{Math.round(weather.main.temp)}°C</div>
+                <div className="text-xs text-slate-600 inline-block mr-4 mt-3"><span className='text-xxs'>Min:</span>{weather.main.temp_min}°C</div>
+                <div className="text-xs  text-slate-600 inline-block"><span className='text-xxs'>Max:</span>{weather.main.temp_max}°C</div>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather font-medium font-mono text-3xl text-orange-950 tracking-wide mt-9">{weather.weather[0].main}</div>
         </div>
-        ): ('Please enter Correct place name!')}
+        ): ("")}
         
     </main>
     </div>
